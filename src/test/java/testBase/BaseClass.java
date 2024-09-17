f2e00b779a241dc55dd199ebd6ca73ead5f7e032
DeepakTDK <deepakdheepu25@gmail.com>
2024-09-17 13:02:56 +0530

    added loggers

--- src/test/java/testBase/BaseClass.java
+++ src/test/java/testBase/BaseClass.java
@@ -1,18 +1,33 @@
 package testBase;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
 
 import java.time.Duration;
 
 public class BaseClass {
-    WebDriver driver;
+    public WebDriver driver;
+    public Logger logger;
 
     @BeforeClass
-    void setup(){
-        driver = new ChromeDriver();
+    @Parameters({"os", "browser"})
+    public void setup(String os, String br){
+        logger = LogManager.getLogger(this.getClass());
+
+        switch (br.toLowerCase()){
+            case "chrome" : driver = new ChromeDriver(); break;
+            case "firefox" : driver = new FirefoxDriver(); break;
+            case "edge" : driver = new EdgeDriver(); break;
+            default: System.out.println("Invalid browser name"); return;
+        }
+
         driver.manage().deleteAllCookies();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
@@ -21,7 +36,7 @@ public class BaseClass {
     }
 
     @AfterClass
-    void teardown(){
+    public void teardown(){
         driver.quit();
     }
 }
---
+++ src/test/java/testCases/TC001_AccountRegistrationTest.java
@@ -0,0 +1,46 @@
+package testCases;
+
+import org.apache.commons.lang3.RandomStringUtils;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pageObjects.AccountRegistrationPage;
+import pageObjects.HomePage;
+import testBase.BaseClass;
+
+public class TC001_AccountRegistrationTest extends BaseClass {
+    @Test
+    void verify_account_registration(){
+        logger.info("Started TC001_AccountRegistrationTest");
+        logger.debug("This is a debug log message");
+        try {
+            HomePage hp = new HomePage(driver);
+            hp.clickMyAccount();
+            logger.info("Clicked on MyAccount Link");
+            hp.clickRegister();
+            logger.info("Clicked on Register Link.. ");
+
+            AccountRegistrationPage arp = new AccountRegistrationPage(driver);
+            logger.info("Providing customer details");
+            arp.setFirstName("Deepak");
+            arp.setLastName("TDK");
+            arp.setEmail(randomString() + "@gmail.com");
+            arp.setTelephone("882881727");
+            arp.setPassword("Thamandk25.");
+            arp.setConfirmPassword("Thamandk25.");
+            arp.setPrivacyPolicy();
+            arp.clickContinue();
+            logger.info("Validating expected message");
+
+            String confmsg = arp.getConfirmationMsg();
+            Assert.assertEquals(confmsg, "Your Account Has Been Created!", "Confirmation message mismatch");
+            logger.info("Test passed");
+        } catch (Exception e){
+            Assert.fail();
+        }
+    }
+
+    String randomString(){
+        String generatedString = RandomStringUtils.randomAlphabetic(5);
+        return generatedString;
+    }
+}
